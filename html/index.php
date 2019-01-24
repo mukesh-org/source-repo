49028608a8d2710b8f28d5c4a54fa12f41213a43
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-01-24 19:51:19 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -5,7 +5,7 @@
 <h1>This is a PHP page using Skaffold Docker image and FLUX</h1>
 
 <?php
-echo "Hello Kubernetes! PR job!!!---job test 321 /test";
+echo "Hello Kubernetes! PR job!!!---job test 123 /test";
 ?> 
   
 </body>
