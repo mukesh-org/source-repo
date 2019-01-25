eb0b77aff0834e656cf3a1e686fb1a9efa38a01c
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-01-25 21:09:20 +0530

    Update git-sourcemerge.sh

--- git-sourcemerge.sh
+++ git-sourcemerge.sh
@@ -7,15 +7,23 @@ cp /secrets/repo-key/ssh-secret /root/.ssh/id_rsa1
 chmod 600 /root/.ssh/id_rsa1
 ssh-add ~/.ssh/id_rsa1
 
-git remote add origin git@github.com:mukesh-org/$REPO_NAME.git
+cat <<\EOF >> ~/.ssh/config
+#source repo
+Host github.com-mukesh-org/"$REPO_NAME"
+	HostName github.com
+	User git
+	IdentityFile ~/.ssh/id_rsa1
+EOF
+
+git remote add origin git@github.com:mukesh-org/"$REPO_NAME".git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 
-Branch=development
+branch=development
 
 ## resolve any merge conflicts if there are any
 git fetch origin master
 git merge FETCH_HEAD
 
 git checkout master
-git merge --no-ff "$Branch"
+git merge --no-ff "$branch"
 git push -u origin master
