022c23267284ce9e6d835aa45bdc94fd9dea52ff
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-01-25 15:10:09 +0530

    Create git-sourcemerge.sh

---
+++ git-sourcemerge.sh
@@ -0,0 +1,14 @@
+### Shell script for performing GitHub merge commands to master branch ###
+
+#!/bin/sh
+
+git remote add origin git@github.com:mukesh-org/$REPO_NAME.git
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+PR=$PULL_NUMBER
+## resolve any merge conflicts if there are any
+git fetch origin master
+git merge FETCH_HEAD
+
+git checkout master
+git merge --no-ff development
