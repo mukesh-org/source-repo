1ef315bd0ed178f78411189a0e9528dc15e42cae
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-01-25 20:06:40 +0530

    Update git-sourcemerge.sh

--- git-sourcemerge.sh
+++ git-sourcemerge.sh
@@ -1,6 +1,23 @@
-### Shell script for performing GitHub merge commands to master branch ###
-
 #!/bin/sh
+
+mkdir /root/.ssh
+chmod 700 /root/.ssh
+cp /secrets/flux/id_rsa /root/.ssh/id_rsa
+cp /secrets/repo-key/ssh-secret /root/.ssh/id_rsa1
+chmod 600 /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa1
+ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa1
+
+Host me.github.com
+    HostName github.com
+    PreferredAuthentications publickey
+    IdentityFile ~/.ssh/me_rsa
+
+Host work.github.com
+    HostName github.com
+    PreferredAuthentications publickey
+    IdentityFile ~/.ssh/work_rsa
 
 git remote add origin git@github.com:mukesh-org/$REPO_NAME.git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
