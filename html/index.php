6df501b2eae839f813561ec5893de445cc224ea3
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-02-07 22:58:30 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1> Development branch into master merge 64454545 </h1>
+<h1> Development branch into master merge 6655 </h1>
 
 <?php
 echo "Hello Kubernetes! PR presubmit job jobtest!!!";
