05283dbe4d314819a4140b920316a3791b0de824
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-02-08 17:28:02 +0530

    Update clonetarget.sh

--- clonetarget.sh
+++ clonetarget.sh
@@ -3,15 +3,15 @@
 mkdir /root/.ssh
 chmod 700 /root/.ssh
 
-cp /secrets/flux/id_rsa /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-ssh-add /root/.ssh/id_rsa
+cp /secrets/git/target_ssh_secret /root/.ssh/target_ssh_secret
+chmod 600 /root/.ssh/target_ssh_secret
+ssh-add /root/.ssh/target_ssh_secret
 
 cat <<\EOF >> ~/.ssh/config
 Host $TARGET_REPO_NAME github.com
 	HostName github.com
 	User git
-	IdentityFile ~/.ssh/id_rsa
+	IdentityFile ~/.ssh/target_ssh_secret
 EOF
 chmod 400 ~/.ssh/config
 
