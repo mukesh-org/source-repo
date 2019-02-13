22d9a3c9674a87968aa2227601994d2f5b833921
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-02-13 18:08:37 +0530

    Update clonetarget.sh

--- scripts/clonetarget.sh
+++ scripts/clonetarget.sh
@@ -2,6 +2,8 @@
 set -e
 
 TARGET_KEY_NAME=$(echo "$TARGET_SSH_PATH" | awk -F/ '{print $NF}')
+export TARGET_KEY_NAME
+
 cp "$TARGET_SSH_PATH" ~/.ssh/"$TARGET_KEY_NAME"
 eval "$(ssh-agent)"
 ssh-add ~/.ssh/"$TARGET_KEY_NAME"
