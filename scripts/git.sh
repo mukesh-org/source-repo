065eca5ed4b6aef05ef6aa2198e4381b29ba3eac
mukesh-panigrahi <mukesh.panigrahi@searce.com>
2019-02-15 21:13:39 +0530

    script updated

--- scripts/git.sh
+++ scripts/git.sh
@@ -7,10 +7,13 @@ REPO_DIR=../"$TARGET_REPO_NAME"
 cd "${REPO_DIR}" || exit
 # echo "$PULL_REFS" > Pull_refs.txt
 echo "$PULL_REFS"
-branch=source-PR-"$PULL_NUMBER"
 
+branch=source-PR-"$PULL_NUMBER"
 git checkout -b "$branch"
+git pull origin "$branch"
+
 git add .
+git status
 git commit -m 'kustomize file and PULL_REF updated'
 git push origin "$branch"
 echo "Code pushed successfully"
