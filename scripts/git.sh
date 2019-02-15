8d94d6f1d309555787c07ae4513980a11e22db02
mukesh-panigrahi <mukesh.panigrahi@searce.com>
2019-02-15 21:29:45 +0530

    sh updated

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,13 +5,14 @@ set -e
 
 REPO_DIR=../"$TARGET_REPO_NAME"
 cd "${REPO_DIR}" || exit
-# echo "$PULL_REFS" > Pull_refs.txt
-echo "$PULL_REFS"
+
+echo "$PULL_REFS" > Pull_refs.txt
+echo Actual-pull-ref="$PULL_REFS"
+echo Pull_refs="$(cat Pull_refs.txt)"
 
 branch=source-PR-"$PULL_NUMBER"
-git checkout -b "$branch"
-git pull origin "$branch"
 
+git checkout -b "$branch"
 git add .
 git status
 git commit -m 'kustomize file and PULL_REF updated'
