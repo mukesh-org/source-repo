5e8a9328e4fef4ee245972d5d5c2720dce87f134
mukesh-panigrahi <mukesh.panigrahi@searce.com>
2019-02-19 02:06:41 +0530

    sh update

--- scripts/mergetarget.sh
+++ scripts/mergetarget.sh
@@ -5,13 +5,10 @@ set -e
 
 REPO_DIR=../"$TARGET_REPO_NAME"
 cd "${REPO_DIR}" || exit
-echo "$PULL_REFS" > Pull_refs.txt
 
 branch=source-PR-"$PULL_NUMBER"
 
 ## resolve any merge conflicts if there are any
-git checkout "$branch"
-git pull
 git checkout master
 git merge --no-ff --no-commit "$branch"
 git status
