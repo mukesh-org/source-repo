b3cb4c1148eddf991c27cf671f197f5f0f341b96
mukesh-panigrahi <mukesh.panigrahi@searce.com>
2019-02-18 14:15:07 +0530

    sh updated

--- scripts/buildsource.sh
+++ scripts/buildsource.sh
@@ -3,7 +3,11 @@ set -e
 
 gcloud auth activate-service-account --key-file /secrets/gcloud/skaffold-secret.json
 gcloud auth configure-docker
-#need to get cluster credentials.
+
+./clonetarget.sh
+
+REPO_DIR=../"$REPO_NAME"
+cd "${REPO_DIR}" || exit
+
 sed -i "s/SKAFFOLD_BUCKET_NAME/${skaffold_bucket_name}/g" skaffold.yaml
-./scripts/clonetarget.sh
 skaffold run
