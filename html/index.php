2c3499d5a1dfe83551bd4551b8ef5e1c9806f381
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-02-19 17:59:49 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -3,7 +3,7 @@
 <body>
 
 <h1> Development branch into master merge shell check no common </h1>
-  <h2> Dev branch into master merge shell check new edit1231234444</h2>
+  <h2> Dev branch into master merge shell check new edit1231234444-new check </h2>
 
 <?php
 echo "Hello Kubernetes! PR presubmit job jobtest!!!";
