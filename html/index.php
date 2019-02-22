77a674beea9d43c2e112bee4f0e8e39bc358bdeb
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-02-22 18:16:51 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -3,11 +3,11 @@
 <body>
 
 <h1> Development branch into master merge shell check no common </h1>
-  <h2> Dev branch into master merge shell check naaa </h2>
+  <h2> Dev branch into master merge shell check </h2>
 
 <?php
 echo "Hello Kubernetes! PR presubmit job jobtest!!!";
-  echo "Hello Kubernetes! PR presubmit job jobtest for Dev branch 22222!!!";
+  echo "Hello Kubernetes! PR presubmit job jobtest for Dev branch!!!";
 ?> 
   
 </body>
