c5443bf5623a821366e21bdd8cc169b32837b898
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-02-27 22:38:04 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -3,7 +3,7 @@
 <body>
 
 <h1> Development branch into master merge shell check no </h1>
-  <h2> Dev branch into master merge shell check fff-test </h2>
+  <h2> Dev branch into master merge shell check </h2>
 
 <?php
 echo "Hello Kubernetes! PR presubmit job fast-forward check";
