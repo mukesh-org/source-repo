3b23b945c0796acdd7f4b9ae4ed07a63973d17fe
mukesh-panigrahi <mukesh.panigrahi@searce.com>
2019-04-08 00:42:48 +0530

    file updated

--- html/demo.php
+++ html/demo.php
@@ -2,8 +2,8 @@
 <html>
 <body>
 
-<h1> Development branch into master branch </h1>
-<font color="green">Dev bar, give me green</font>
+<h1> Sample branch into master branch </h1>
+<h1> Dev bar, my color is Black </h1>
 
 <?php
 echo "Dev Jack, Increase my font";
