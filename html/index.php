f06c654c1ed35d79a073321e98868577871ae57e
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-04-07 22:51:39 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -3,7 +3,7 @@
 <body>
 
 <h1> Development branch into master branch </h1>
-  <h2> Dev foo, give me red</h2>
+<font color="red">Dev foo, give me red</font>
 
 <?php
 echo "Dev Jack, Increase my font";
