30b108dc280ff11ce4004376ced69b2a34f33153
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-04-08 17:59:43 +0530

    Update demo.php

--- html/demo.php
+++ html/demo.php
@@ -3,7 +3,7 @@
 <body>
 
 <h1> Sample branch into master branch </h1>
-<h1> Dev bar, my color is Green now </h1>
+<h2> Dev bar, my color is Green </h2>
 
 <?php
 echo "Dev Jack, Decreased the above font";
