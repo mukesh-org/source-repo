a1e734f040d18783abe5cc701caf511ad32f2dd2
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-04-23 01:35:12 +0530

    Update demo.php

--- html/demo.php
+++ html/demo.php
@@ -3,10 +3,10 @@
 <body>
 
 <h1> I am Demo.php </h1>
-<h2> Dev Bar, my color is BLUE </h2>
+<h2> Dev Bar, my color is WHITE </h2>
 
 <?php
-echo "Dev Jack, my color is YELLOW";
+echo "Dev Jack, my color is GREYY";
 ?> 
   
 </body>
