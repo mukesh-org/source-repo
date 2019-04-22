45ebd03eab57a6925c5b99a5ef6b758713fdcf71
mukesh-panigrahi <mukesh.panigrahi@searce.com>
2019-04-23 02:14:33 +0530

    minor update

--- scripts/mergetarget.sh
+++ scripts/mergetarget.sh
@@ -8,19 +8,20 @@ cd "${REPO_DIR}" || exit
 branch=source-PR-"$PULL_NUMBER"
 
 target_pull_refs="$(cat Pull_refs.txt)"
-sleep 70
+echo sleep 60 sec
+sleep 60
 
 if [ "$PULL_REFS" == "$target_pull_refs" ]
 then
     echo "PR-REF matched successfully. Proceeding with $branch merge"
     git checkout master
-    git merge --ff-only origin $branch || echo "cannot fast-forward"
+  { git merge --ff-only origin $branch } || { echo "cannot fast-forward" && exit 1 }
     git status
     git push -u origin master
     echo "$branch Merged successfully to master"
 else
     echo $branch-PR_REF="$target_pull_refs"
     echo Actual-PR_REF="$PULL_REFS"
-    echo "PR-REF didn't match. either wait for build to finish or rerun the build-job followed by /test job"
+    echo "PR-REF didn't match. either wait for build to finish or rerun the build-job followed by /approve"
     exit 1
 fi
