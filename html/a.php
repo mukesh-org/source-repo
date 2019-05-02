5c747cf066bf7ef6ad938ce96f29008d76931feb
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-05-03 00:22:28 +0530

    Update a.php

--- html/a.php
+++ html/a.php
@@ -3,9 +3,9 @@
 <body>
 
 <h1> I am a.php </h1>
-<h2> Give me Red </h2>
+<h2> Give me Magenta </h2>
 <?php
-echo "Dev A, my fav color is RED";
+echo "Dev A, my fav color is MAGENTA";
 ?> 
   
 </body>
